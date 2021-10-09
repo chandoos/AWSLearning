0648ee3f0f9a8e7da9cff3d742bd359800e79f12
Chandu <chandra.ava@gmail.com>
2021-10-09 16:40:49 -0400

    adding s3 bucket creation stack

--- bin/hello-world-cloud.ts
+++ bin/hello-world-cloud.ts
@@ -15,7 +15,7 @@ new HelloWorldCloudStack(app, 'HelloWorldCloudStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  //env: { account: '358351935223', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
